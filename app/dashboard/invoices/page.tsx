4e214389da8426eeb10255919b6557d87d7cd2c9
Marcos Augusto <marcosdeantonio2020@gmail.com>
2025-12-28 15:12:46 -0300

    add

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>Welcome to Invoices Dashboard Page</h1>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <p>Welcme to Dashboard Page</p>;
+}
